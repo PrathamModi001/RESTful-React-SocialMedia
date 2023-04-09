262c617b94e7f9efc548f45dff2f1fecde358f34
PrathamModi001 <prathammodi001@gmail.com>
2023-04-09 17:06:27 +0530

    updating posts logic with socket.io

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,6 +2,7 @@ const { validationResult } = require('express-validator');
 const fs = require('fs');
 const path = require('path')
 
+const io = require('../socket');
 const Post = require('../models/post');
 const User = require('../models/user')
 const fileHelper = require('../util/file')
@@ -12,7 +13,7 @@ exports.getPosts = async (req, res, next) => {
         const perPage = 2;
         let totalItems;
         totalItems = await Post.find().countDocuments()
-        const posts = await Post.find().skip((currentPage - 1) * perPage).limit(perPage)
+        const posts = await Post.find().populate('creator').skip((currentPage - 1) * perPage).limit(perPage)
 
         return res.status(200).json({
             message: 'Posts Fetched Successfully',
@@ -60,6 +61,13 @@ exports.postPosts = async (req, res, next) => {
         userFound.posts.push(newPost);
         await userFound.save();
 
+        // inform all other users about the post we just created using socket.io
+        io.getIO().emit('posts', { // can think like channel: posts
+            action: 'create', // what are we doing? create.NOT NECESSARY FOR SOCKET.IO
+            post: { ...newPost._doc, creator: { _id: req.userId, name: userFound.name } }
+            // passing on the entire newPost document
+        })
+
         return res.status(201).json({
             message: 'Post creation was successful!',
             post: newPost,
@@ -98,64 +106,66 @@ exports.getSinglePost = async (req, res, next) => {
     }
 };
 
-exports.updatePost = (req, res, next) => {
-    const postId = req.params.postId;
+exports.updatePost = async (req, res, next) => {
+    try {
+        const postId = req.params.postId;
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const error = new Error('Input Validation Failed.');
-        error.statusCode = 422;
-        throw error;
-    }
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const error = new Error('Input Validation Failed.');
+            error.statusCode = 422;
+            throw error;
+        }
 
-    const title = req.body.title;
-    const content = req.body.content;
-    let imageUrl = req.body.image; // not updating the image;
+        const title = req.body.title;
+        const content = req.body.content;
+        let imageUrl = req.body.image; // not updating the image;
 
-    if (req.file) { // a new file was picked 
-        imageUrl = req.file.path
-    }
+        if (req.file) { // a new file was picked 
+            imageUrl = req.file.path
+        }
 
-    if (!imageUrl) { // if there is no imageUrl till now, throw an error
-        const error = new Error('No File Picked!')
-        error.statusCode = 422;
-        throw error;
-    }
+        if (!imageUrl) { // if there is no imageUrl till now, throw an error
+            const error = new Error('No File Picked!')
+            error.statusCode = 422;
+            throw error;
+        }
 
-    Post.findById(postId)
-        .then(post => {
-            if (!post) {
-                const error = new Error('Post Not Found')
-                error.statusCode = 404;
-                throw error;
-            }
-            if (post.creator.toString() !== req.userId) {
-                const error = new Error('You are NOT the creator of this post!')
-                error.statusCode = 403;
-                throw error;
-            }
-            // deleting old image if we select a new one
-            if (imageUrl !== post.imageUrl) {
-                fileHelper.deleteFile(post.imageUrl);
-            }
+        const post = await Post.findById(postId).populate('creator')// we are storing creator in every post, and so by populating it, we can have that users ENTIRE data as well.
+        if (!post) {
+            const error = new Error('Post Not Found')
+            error.statusCode = 404;
+            throw error;
+        }
+        // since we populated the creator to be the entire user in the top, creator._id
+        if (post.creator._id.toString() !== req.userId) {
+            const error = new Error('Not authorized!');
+            error.statusCode = 403;
+            throw error;
+        }
+        // deleting old image if we select a new one
+        if (imageUrl !== post.imageUrl) {
+            fileHelper.deleteFile(post.imageUrl);
+        }
 
-            post.title = title;
-            post.content = content;
-            post.imageUrl = imageUrl
-            return post.save()
+        post.title = title;
+        post.content = content;
+        post.imageUrl = imageUrl
+        const result = await post.save()
+
+        io.getIO().emit('posts', { action: 'update', post: result }) // because we did populate creator up above, we are also sending user data through this post: result
+
+        return res.status(200).json({
+            message: 'Post Updated Successfully!',
+            post: result
         })
-        .then(result => {
-            res.status(200).json({
-                message: 'Post Updated Successfully!',
-                post: result
-            })
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err)
-        })
+    }
+    catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err)
+    }
 }
 
 exports.deletePost = async (req, res, next) => {
--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -41,7 +41,16 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket('http://localhost:8080');
+    const socket = openSocket('http://localhost:8080');
+    // through the feed.js controller we know we are sending posts into the 'posts channel
+    socket.on('posts', data => {
+      if (data.action === 'create') {
+        this.addPost((data.post))
+      }
+      else if(data.action === 'update'){
+        this.updatePost((data.post))
+      }
+    })
   }
 
   addPost = (post) => {
@@ -58,8 +67,20 @@ class Feed extends Component {
         totalPosts: prevState.totalPosts + 1
       };
     });
-
   }
+
+  updatePost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts
+      };
+    });
+  };
 
   // loading ALL posts 
   loadPosts = direction => {
@@ -183,18 +204,8 @@ class Feed extends Component {
           creator: resData.post.creator,
           createdAt: resData.post.createdAt
         };
-        this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
-          }
+        this.setState(prevState => {          
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
