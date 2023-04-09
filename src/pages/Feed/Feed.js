6a7107e5c463a2fd1091cdf49e4d0e4f0b6aa51c
PrathamModi001 <prathammodi001@gmail.com>
2023-04-09 11:57:13 +0530

    added socket io module stuff and user is connected

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import openSocket from 'socket.io-client';
 
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
@@ -21,6 +22,7 @@ class Feed extends Component {
     editLoading: false
   };
 
+  // load our posts
   componentDidMount() {
     fetch('http://localhost:8080/auth/status', {
       headers: {
@@ -39,6 +41,7 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+    openSocket('http://localhost:8080');
   }
 
   // loading ALL posts 
